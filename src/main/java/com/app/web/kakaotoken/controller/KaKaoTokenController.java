744f417bfa7e383d00d591280258da7da65868b1
dongjun6343 <dongjun6343@naver.com>
2023-04-20 21:24:30 +0900

    # kakao 토큰 발급 구현  토큰 받기

--- src/main/java/com/app/web/kakaotoken/controller/KaKaoTokenController.java
+++ src/main/java/com/app/web/kakaotoken/controller/KaKaoTokenController.java
@@ -1,6 +1,9 @@
 package com.app.web.kakaotoken.controller;
 
+import com.app.web.kakaotoken.client.KakaoTokenClient;
+import com.app.web.kakaotoken.dto.KakaoTokenDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +12,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class KaKaoTokenController {
 
+    private final KakaoTokenClient kakaoTokenClient;
+
+    @Value("${kakao.client.id}")
+    private String clientId;
+
+    @Value("${kakao.client.secret}")
+    private String clientSecret;
+
     @GetMapping("/login")
     public String login(){
         return "loginForm";
@@ -16,7 +27,18 @@ public class KaKaoTokenController {
 
     @GetMapping("/oauth/kakao/callback")
     public @ResponseBody String loginCallback(String code){
-        return code;
+        String contentType = "application/x-www-form-urlencoded;charset=utf-8";
+        KakaoTokenDto.Request kakaoTokenRequestDto = KakaoTokenDto.Request.builder()
+                .client_id(clientId)
+                .client_secret(clientSecret)
+                .grant_type("authorization_code")
+                .code(code)
+                .redirect_uri("http://localhost:8080/oauth/kakao/callback")
+                .build();
+
+        KakaoTokenDto.Response kakaoToken = kakaoTokenClient.requestKakaoToken(contentType, kakaoTokenRequestDto);
+
+        return "kakao token : " + kakaoToken;
     }
 
 }
